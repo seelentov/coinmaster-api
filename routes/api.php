109bdc0614d9a1b53103a59e44ee6663d50005e7
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-09-06 17:05:35 +0500

    dublicate moonshine assets

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group([
-    'namespace' => "App\Http\Controllers"
+    'namespace' => "App\Http\Controllers",
 ], function () {
     Route::group(
         [
