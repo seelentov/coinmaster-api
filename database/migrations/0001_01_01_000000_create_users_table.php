68582700d194d78642f653a4bac9843cdf74d6b0
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-09-10 18:00:41 +0500

    upd avatar url user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('avatar_url')->default("/storage/images/default_avatar.jpg");
+            $table->string('avatar_url')->default("/uploaded/default_avatar.jpg");
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->timestamp('user_verified_at')->nullable();
