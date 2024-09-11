30b7c18baf876251f5952afea53fd1df8a6daed8
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-09-11 21:18:58 +0500

    Update ValuteService.php

--- app/Services/ValuteService.php
+++ app/Services/ValuteService.php
@@ -124,20 +124,23 @@ class ValuteService extends AbstractService implements IValuteService
 
     private function getCounts(&$res, $data)
     {
-        $res["value"] = floatval(str_replace(",", ".", $data["Value"])) * intval($data["Nominal"]);
+        if (isset($data["Value"]) && isset($data["Previous"])) {
 
-        $res["prev_value"] = floatval(str_replace(",", ".", $data["Previous"])) * intval($data["Nominal"]);
+            $res["value"] = floatval(str_replace(",", ".", $data["Value"])) * intval($data["Nominal"]);
 
-        $res["rise"] = $res["value"] - $res["prev_value"];
+            $res["prev_value"] = floatval(str_replace(",", ".", $data["Previous"])) * intval($data["Nominal"]);
 
-        if ($res["prev_value"] != 0) {
-            $res["rise_percent"] = ($res["rise"] / $res["prev_value"]) * 100;
-        } else {
-            $res["rise_percent"] = 0;
+            $res["rise"] = $res["value"] - $res["prev_value"];
+
+            if ($res["prev_value"] != 0) {
+                $res["rise_percent"] = ($res["rise"] / $res["prev_value"]) * 100;
+            } else {
+                $res["rise_percent"] = 0;
+            }
+
+            $res["rise"] = round($res["rise"], 4);
+            $res["rise_percent"] = round($res["rise_percent"], 4);
         }
-
-        $res["rise"] = round($res["rise"], 4);
-        $res["rise_percent"] = round($res["rise_percent"], 4);
     }
 
     private function serializeValute($data)
