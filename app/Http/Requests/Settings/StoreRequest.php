07c55eb163666eb874a2fb9b302d4d962eb394e8
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-09-12 17:12:04 +0500

    upd Settings req, add time

--- app/Http/Requests/Settings/StoreRequest.php
+++ app/Http/Requests/Settings/StoreRequest.php
@@ -9,7 +9,7 @@ class StoreRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            "notif_time" => "time",
+            "notif_time" => 'date_format:H:i:s',
             "notif_active" => "boolean",
             "lang" => 'in:' . implode(',', Settings::getLangs()),
         ];
--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -6,6 +6,10 @@ use App\Models\Abstract\AbstractModel;
 
 class Settings extends AbstractModel
 {
+    protected $hidden = [
+        'user_id',
+        'id',
+    ];
     const LANG_RU = 'ru';
 
 
